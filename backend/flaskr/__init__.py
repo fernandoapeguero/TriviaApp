4899c7fbd467b7c6f068445ab750b5d6d9fd8df7
fernando peguero <fernandoapeguero@gmail.com>
2021-01-06 01:31:54 -0500

    all endpoint working and the as well as the quizz game

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -14,15 +14,8 @@ def create_app(test_config=None):
   app = Flask(__name__)
   setup_db(app)
   
-  '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
   cors = CORS(app, resources={r"/trivia_api/*": {"origins": "*"}})
 
-
-  '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
   @app.after_request
   def after_request(reponse):
         reponse.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
@@ -30,7 +23,7 @@ def create_app(test_config=None):
         return reponse
         
   QUESTION_COUNT = 10
-
+  # ======================= Helper Functions =======================
   def paginate(request ,selection):
         
         page = request.args.get('page' , 1 , type=int)
@@ -42,14 +35,19 @@ def create_app(test_config=None):
         formatted_questions = questions[start:end]
   
         return formatted_questions
-        
+
+  # gets a random entry from database results 
+  def random_question(questions):
+        if len(questions) > 0:
+              my_question = questions[math.floor(random() * len(questions))]
+
+              return my_question.format()
+        else:
+              return ""
 
 
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests 
-  for all available categories.
-  '''
+  # ===================== End Points ======================
+
   @app.route("/trivia_api/categories")
   def get_categories():
         
@@ -71,19 +69,7 @@ def create_app(test_config=None):
         except:
           abort(400)
 
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
 
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
-  '''
-  
   @app.route('/trivia_api/main/questions')
   def index():
 
@@ -109,15 +95,7 @@ def create_app(test_config=None):
 
       except:
         abort(404)
-        
-        
-  '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
 
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
 
   @app.route('/trivia_api/<int:question_id>/questions' , methods=['DELETE'])
   def delete_question(question_id):
@@ -136,17 +114,6 @@ def create_app(test_config=None):
           })
         except:
           abort(404)
-  '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
-
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
-
 
   @app.route("/trivia_api/questions" , methods=['POST'])
   def post_question():
@@ -174,16 +141,6 @@ def create_app(test_config=None):
 
         except:
           abort(400)
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
-
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-  '''
 
   @app.route('/trivia_api/search_questions' , methods=['POST'])
   def get_question():
@@ -203,14 +160,6 @@ def create_app(test_config=None):
     except:
       abort(422)
 
-  '''
-  @TODO: 
-  Create a GET endpoint to get questions based on category. 
-
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
-  '''
   @app.route('/trivia_api/<int:category_id>/categories')
   def get_questions_by_category(category_id):
         
@@ -230,18 +179,7 @@ def create_app(test_config=None):
           
         except:
           abort(404)
-
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
-
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-  '''
+        
   @app.route("/trivia_api/quizzes" , methods=['POST'])
   def get_quizz_questions():
         
@@ -256,28 +194,22 @@ def create_app(test_config=None):
           current_question = ''
           
           if quizz_category == 0:
-              current_question = Question.query.filter(Question.id.notin_(questions_tupple)).first()
+
+              questions = Question.query.filter(Question.id.notin_(questions_tupple)).all()
+              current_question = random_question(questions)
           else: 
-              current_question = Question.query.filter(Question.category == quizz_category , Question.id.notin_(questions_tupple)).first()
 
-
-          if current_question is None:
-                current_question = ''
-                
+              questions = Question.query.filter(Question.category == quizz_category , Question.id.notin_(questions_tupple)).all()
+              current_question = random_question(questions)
 
           return jsonify({
             "success": True,
-            "question": current_question.format() 
+            "question": current_question 
           })
         except:
           abort(404)
 
-  '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
-  # 400, 404, 422 and 500
+  # =========== Error Hanlders ====================
 
   @app.errorhandler(400)
   def bad_request(error):
